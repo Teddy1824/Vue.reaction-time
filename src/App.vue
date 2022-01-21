262dfa5e287926ed41de44a7fa9cd044a97238fb
Bongani <bearnomangola7@gmail.com>
2022-01-21 14:20:50 +0200

    my  first game

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>Bongani's Reaction Timer</h1>
+ <button @click="start" :disabled="isPlaying">Play</button>
+<Block v-if="isPlaying" :delay="delay" @done="endGame"/>
+<Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Block,
+    Results
+  },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
+  },
 }
 </script>
 
@@ -20,7 +47,14 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+body {
+  background-image: url(https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
+  background-size: cover;
+  background-repeat: no-repeat;
+  object-fit: cover;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,53 @@
+<template>
+        <p>Reaction time - {{ score }}</p>
+        <p class="speed">{{ speed }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            speed: null
+        }
+    },
+
+    mounted() {
+        if(this.score < 250) {
+            this.speed = "Very fast"
+        }
+
+        else if (this.score < 400) {
+            this.speed = "Relatively fast"
+        }
+
+        else {
+            this.speed = "Not so fast, try again."
+        }
+    }
+}
+</script>
+
+<style>
+    .speed {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+
+    button {
+        background: #0faf82;
+        color: white;
+        border: none;
+        padding: 8px 16px;
+        border-radius: 4px;
+        letter-spacing: 1px;
+        cursor: pointer;
+        margin: 10px;
+    }
+
+    button[disabled] {
+        opacity: 0.2;
+        cursor: not-allowed;
+    }
+</style>
